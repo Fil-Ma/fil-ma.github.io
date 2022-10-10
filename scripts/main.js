367fbd5499a13649f9c7cd76f6d06e5a1111f46b
Filippo <filippo.mazzalai@gmail.com>
2022-10-10 11:19:40 +0200

    website is now responsive

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,10 @@
+const hamburgerMenu = document.getElementById("hamburger-menu");
 
+// this functions opens hamburger menu if user clicks the icon
+function handleClickHanburger() {
+  if (hamburgerMenu.style.display === "block") {
+    hamburgerMenu.style.display = "none";
+  } else {
+    hamburgerMenu.style.display = "block";
+  }
+};
